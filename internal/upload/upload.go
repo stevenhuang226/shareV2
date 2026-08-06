eb6ff22add5c54be8bc713ad4547c1d6870087f8
PotatoHuang <stevenhuang.dog@gmail.com>
2026-08-06 19:44:40 +0800

    upload manager/session base working

--- internal/upload/upload.go
+++ internal/upload/upload.go
@@ -1,45 +1,98 @@
 package upload
 
 import (
+	"errors"
 	"io"
 	"sharev2/internal/storage"
 	"sync"
 )
 
 type Session struct {
-	mu         sync.Mutex
-	upload     *storage.Upload
-	expectSize int64
+	mu     sync.Mutex
+	upload *storage.Upload
 }
 
 type Manager struct {
 	mu       sync.Mutex // protect sessions
 	sessions map[string]*Session
+	storage  *storage.Storage
+}
+
+func CreateManager(storage *storage.Storage) (*Manager, error) {
+	sessions := make(map[string]*Session)
+
+	return &Manager{
+		sessions: sessions,
+		storage:  storage,
+	}, nil
 }
 
 func (m *Manager) NewSession() (string, error) {
-	/*
-		create new session
+	u, err := m.storage.CreateUpload()
+	if err != nil {
+		return "", err
+	}
 
-		write with sync lock
-	*/
+	session := &Session{
+		upload: u,
+	}
+
+	m.mu.Lock()
+	m.sessions[u.Id()] = session
+	m.mu.Unlock()
+
+	return u.Id(), nil
 }
 
-func (s *Session) Append(offset int64, r io.Reader) (int64, error) {
+func (m *Manager) Append(id string, offset int64, r io.Reader) (int64, error) {
+	session, err := m.findSession(id)
+	if err != nil {
+		return 0, err
+	}
+
+	return session.append(offset, r)
+}
+
+func (m *Manager) Commit(id string) error {
+	session, err := m.findSession(id)
+	if err != nil {
+		return err
+	}
+
+	m.mu.Lock()
+	delete(m.sessions, id)
+	m.mu.Unlock()
+
+	return session.commit()
+}
+
+func (m *Manager) findSession(id string) (*Session, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	session, ok := m.sessions[id]
+	if !ok {
+		return nil, errors.New("session not found")
+	}
+
+	return session, nil
+}
+
+func (s *Session) append(offset int64, r io.Reader) (int64, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	return s.upload.Append(offset, r)
 }
 
-func (s *Session) Commit() error {
+func (s *Session) commit() error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	return s.upload.Commit()
 }
 
-func (s *Session) Abort() error {
+func (s *Session) abort() error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
